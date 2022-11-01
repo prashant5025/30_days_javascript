8274ba4638700b527539c3af9a49ce1d641a952a
Prashant Singh <psingh5025615@gmail.com>
2022-11-01 05:45:37 +0530

    initial commit

--- json.js
+++ json.js
@@ -1,4 +1,129 @@
 const user = {
+  Alex: {
+    email: "alex@alex.com",
+    skills: ["HTML", "CSS", "JavaScript"],
+    age: 20,
+    isLoggedIn: false,
+    points: 30,
+  },
+  Asab: {
+    email: "asab@asab.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Redux",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node",
+    ],
+    age: 25,
+    isLoggedIn: false,
+    points: 50,
+  },
+  Brook: {
+    email: "daniel@daniel.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux"],
+    age: 30,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Daniel: {
+    email: "daniel@alex.com",
+    skills: ["HTML", "CSS", "JavaScript", "Python"],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  John: {
+    email: "john@john.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux", "Node.js"],
+    age: 20,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Thomas: {
+    email: "thomas@thomas.com",
+    skills: ["HTML", "CSS", "JavaScript", "React"],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  Paul: {
+    email: "paul@paul.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node",
+    ],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+};
+
+const usersText = `{
+    "users":[
+      {
+        "firstName":"Asabeneh",
+        "lastName":"Yetayeh",
+        "age":250,
+        "email":"asab@asb.com"
+      },
+      {
+        "firstName":"Alex",
+        "lastName":"James",
+        "age":25,
+        "email":"alex@alex.com"
+      },
+      {
+      "firstName":"Lidiya",
+      "lastName":"Tekle",
+      "age":28,
+      "email":"lidiya@lidiya.com"
+      }
+    ]
+    }`;
+
+// 1. Find the person who has many skills in the users object.
+
+// const userWithMostSkills = Object.entries(user).reduce((acc, [key, value]) => {
+//     if(acc.length < value.skills.length){
+//         acc = key
+//     }
+//     return acc
+// })
+
+// const sortData = Object.entries(user).sort((a, b) => b[1].skills.length - a[1].skills.length)
+
+// sortData.map((item) => {
+//     console.log(item)
+// })
+
+// const usersObj = JSON.parse(usersText,(key, value) => {
+//     let newValue = typeof value == 'string' && key != 'email' ? value.toUpperCase() : value
+//     return newValue
+// })
+
+// const txt = JSON.stringify(user,['firstName', 'lastName', 'country', 'city', 'age'],4)
+// console.log(txt)
+
+const skills = ["HTML", "CSS", "JS", "React", "Node", "Python"];
+let age = 250;
+let isMarried = true;
+const student = {
+  firstName: "Asabeneh",
+  lastName: "Yetayehe",
+  age: 250,
+  isMarried: true,
+  skills: ["HTML", "CSS", "JS", "React", "Node", "Python"],
+};
+const txt = `{
     "Alex": {
         "email": "alex@alex.com",
         "skills": [
@@ -93,14 +218,25 @@ const user = {
         "points": 40
     }
 }
-    
-// 1. Find the person who has many skills in the users object.
+`;
 
-const userWithMostSkills = Object.entries(user).reduce((acc, [key, value]) => {
-    if(acc.length < value.skills.length){
-        acc = key
-    }
-    return acc
-})
+const jsonToArray = JSON.stringify(skills, undefined, 4);
+const ageJson = JSON.parse(age);
+const jsonisMarried = JSON.stringify(isMarried, undefined, 4);
+console.log(jsonToArray);
+console.log(ageJson);
 
-const sortData = Object.entries(user).sort((a, b) => b[1].skills.length - a[1].skills.length)
+console.log(jsonisMarried);
+
+const JsonSTudent = JSON.stringify(student, undefined, 4);
+const JsonSTudentParse = JSON.stringify(
+  student,
+  ["firstName", "lastName", "skills"],
+  4
+);
+console.log(JsonSTudent);
+console.log(JsonSTudentParse);
+
+const uri = "https://jsonplaceholder.typicode.com/users";
+
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,18 @@
-return obj
+const userWithMostSkills = Object.entries(user).reduce((acc, [key, value]) => {
+//     if(acc.length < value.skills.length){
+//         acc = key
+//     }
+//     return acc
+// })
+
+// const sortData = Object.entries(user).sort((a, b) => b[1].skills.length - a[1].skills.length)
+
+// sortData.map((item) => {
+//     console.log(item)
+// })
+
+
+// const usersObj = JSON.parse(usersText,(key, value) => {
+//     let newValue = typeof value == 'string' && key != 'email' ? value.toUpperCase() : value 
+//     return newValue
+// })
